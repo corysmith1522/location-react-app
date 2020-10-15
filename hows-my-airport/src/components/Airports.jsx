6fed4825df58a17898a19b91e551e48da9b6417e
Cory Smith <cory.smith1522@outlook.com>
2020-10-15 13:13:46 -0400

    important links

--- hows-my-airport/src/components/Airports.jsx
+++ hows-my-airport/src/components/Airports.jsx
@@ -11,17 +11,25 @@ function Airports() {
   const landing = 'https://bit.ly/2SUEOx9'
 
   return (
-    <div className="layout">
-      <div className="list">
-        <List />
+    <div>
+      <div className="hyperlinks">
+        <h2 className="important-links">THE MOST IMPORTANT LINKS</h2>
+          <h6><a className="other-links" href='https://bit.ly/350ZYzt'>Real-Time Flight Delay Status</a></h6>
+          <h6><a className="other-links" href='https://bit.ly/2GSZaor'>Directions and Traffic</a></h6>
+          <h6><a className="other-links" href='https://bit.ly/3j8AbKU'>Real-Time Weather Status</a></h6>
       </div>
-      <div className="pics">
-        <img src={airplaneIMG} alt="airplane" />
-        <img src={coolIMG} alt="airplane" />
-        <img src={airportGate} alt="airport-gate" />
-        <img src={planeGate} alt="airplane-gate" />
-        <img src={floorshot} alt="airport-concourse" />
-        <img src={landing} alt="airplane-landing" />
+      <div className="layout">
+        <div className="list">
+          <List />
+        </div>
+        <div className="pics">
+          <img src={airplaneIMG} alt="airplane" />
+          <img src={coolIMG} alt="airplane" />
+          <img src={airportGate} alt="airport-gate" />
+          <img src={planeGate} alt="airplane-gate" />
+          <img src={floorshot} alt="airport-concourse" />
+          <img src={landing} alt="airplane-landing" />
+        </div>
       </div>
     </div>
   )
